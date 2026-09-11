d4fbcea5cb12f1c30f1f0638121be30d167f6cba
pang0002 <angelpang0908@gmail.com>
2026-09-11 16:18:40 +0800

    update new type of blog submission

---
+++ submit-config.js
@@ -0,0 +1,5 @@
+// ============================================================
+// ARTICLE SUBMISSION — CONFIG
+// ============================================================
+
+const SUBMIT_ENDPOINT = "https://script.google.com/macros/s/AKfycbyNtlNUXkO7tE96onWxOLVcbgT_7zQxU25MQHMQzeRu6Kok-EV1vHT9Exi_ial0dXPe/exec";
